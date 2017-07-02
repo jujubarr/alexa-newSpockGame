ddae252e4d85c91fee7918a91f97ae09e0b39f0f
Rosemary Chen <rosemary.chen@sap.com>
2017-07-02 15:01:01 -0700

    Speech configurations and typo corrections

--- index.js
+++ index.js
@@ -2,27 +2,26 @@
 
 const Alexa = require('alexa-sdk');
 
-const APP_ID='amzn1.ask.skill.51852cb4-6667-4e60-ad9b-36c8191635d5';
+const APP_ID = 'amzn1.ask.skill.b10d250d-4358-466a-a9b2-4fd5a28db0bb';
 
 var alexa_score = 0, player_score = 0;
-
 const handlers = {
 	'LaunchRequest': function () {
 		this.attributes.speechOutput='Welcome to Rock, Spock, Whatever Game. Please say your command';
-		this.attributes.repromptSpeech = 'Please say your command';
-		ths.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+		this.attributes.repromptSpeech = 'bytch? command please?';
+		this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
 	},
-
+	
+	
 	'NewSpockGameIntent': function () {
-		const itemSlot = this.event.request.intents.slot.Option;
+		const itemSlot = this.event.request.intent.slots.Option;
 		let itemName;
 		if (itemSlot && itemSlot.value) {
 			itemName = itemSlot.value.toLowerCase();
 		}
 
-		if (itemName == 'Thank you') {
-			var dar = "Thanks for playing <break time=\"0.5s\"> Your final score is " +
-				+ player_score + " and my final score is: " + alexa_score;
+		if (itemName == 'thank you') {
+			var data = "Quitting is for losers <break time=\"0.5s\"/> Your final score is " + player_score + " and my final score is: " + alexa_score;
 			alexa_score = 0;
 			player_score = 0;
 			this.emit(':tell', data);
@@ -46,7 +45,7 @@ const handlers = {
 					lizard: 'Scissor decapitates lizard'
 				},
 				lizard: {
-					spock: 'Lizard poos on spock',
+					spock: 'Lizard poisons spock',
 					paper: 'Lizard eats paper'
 				},
 				spock: {
@@ -55,28 +54,28 @@ const handlers = {
 				}
 			};
 
-			if (itemName == alex_word) {
-				response = "Hey! I guess it's a tie!";
+			if (itemName == alexa_word) {
+				response = "Stale mate bra? I guess it's a tie";
 			}
 			else if (word.indexOf(itemName) == -1) {
 				response = itemName + " is not a valid option";
 			}
 			else {
-				response = (alex_word in rules[itemName])
-					? (player_score++, riles[itemName][alex_word] + "<break time=\"0.5s\"/> I guess you won this time")
-					: (alexa_score++, riles[alex_word][itemName] + "<break time=\"0.5s\"/> looks like I won");
+				response = (alexa_word in rules[itemName])
+					? (player_score++, rules[itemName][alexa_word] + "<break time=\"0.5s\"/> Duude. you won")
+					: (alexa_score++, rules[alexa_word][itemName] + "<break time=\"0.5s\"/> looks like I won");
 			}
 
 			this.attributes.speechOutput = response;
-			this.attributes.repromptSpeech = "Let's play again. Say a command";
+			this.attributes.repromptSpeech = "Say a command";
 			this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
 		}
 	}
 };
 
-exports.handler = function(event, context) {
-	const alexa = Alexa.handler(event, context);
-	alexa.APP_ID = APP_ID;
-	alexa.registerHandlers(handlers);
-	alexa.execute();
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
